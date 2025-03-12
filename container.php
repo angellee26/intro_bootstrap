8b4b538559d8d7ca3f1dae01ec71c6acbf82c36a
angellee26 <lcacapol23-0372@cca.edu.ph>
2025-03-12 17:29:27 +0800

    Update container.php

--- container.php
+++ container.php
@@ -1,41 +1,64 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+        <body>
 
-<div class="container">
-  <h1>My First Bootstrap Page</h1>
-  <p>This part is inside a .container class.</p>
-  <p>The .container class provides a responsive fixed width container.</p>
-</div>
+            <div class="container">
+            <h1>My First Bootstrap Page</h1>
+            <p>This part is inside a .container class.</p>
+            <p>The .container class provides a responsive fixed width container.</p>
+            </div>
 
-<div class="container">
-  <p class="h1">h1 Bootstrap heading</p>
-  <p class="h2">h2 Bootstrap heading</p>
-  <p class="h3">h3 Bootstrap heading</p>
-  <p class="h4">h4 Bootstrap heading</p>
-  <p class="h5">h5 Bootstrap heading</p>
-  <p class="h6">h6 Bootstrap heading</p> <br>
-</div>
+            <div class="container">
+                <p class="h1">h1 Bootstrap heading</p>
+                <p class="h2">h2 Bootstrap heading</p>
+                <p class="h3">h3 Bootstrap heading</p>
+                <p class="h4">h4 Bootstrap heading</p>
+                <p class="h5">h5 Bootstrap heading</p>
+                <p class="h6">h6 Bootstrap heading</p> <br>
 
-<div class="container">
-<button type="button" class="btn">Basic</button>
-<button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-dark">Dark</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-link">Link</button>
-</div>
+            </div>
+            <div class="container">
+                <button type="button" class="btn">Basic</button>
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-secondary">Secondary</button>
+                <button type="button" class="btn btn-success">Success</button>
+                <button type="button" class="btn btn-info">Info</button>
+                <button type="button" class="btn btn-warning">Warning</button>
+                <button type="button" class="btn btn-danger">Danger</button>
+                <button type="button" class="btn btn-dark">Dark</button>
+                <button type="button" class="btn btn-light">Light</button>
+                <button type="button" class="btn btn-link">Link</button>
+            </div>
 
-</body>
+            <div class="container">
+            <div class="jumbotron">
+                <h1>Bootstrap Tutorial</h1>
+                <p>Bootstrap is the most popular HTML, CSS...</p>
+            </div>
+
+            <div>
+                <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1>Bootstrap Tutorial</h1>
+                    <p>Bootstrap is the most popular HTML, CSS...</p>
+                
+            </div>
+
+            <div class="mt-4 p-5 bg-primary text-white rounded">
+                <h1>Jumbotron Example</h1>
+                <p>Lorem ipsum...</p>
+            </div>
+
+            
+
+
+
+
+        </body>
 </html>
